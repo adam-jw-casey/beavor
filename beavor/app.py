e8b115415b53ddfe271e748cc194e2247034e099
Adam Casey <adam.jw.casey@gmail.com>
2023-03-24 15:56:11 -0600

    Add main window to display the project

--- beavor/app.py
+++ beavor/app.py
@@ -30,10 +30,16 @@ class WorklistWindow():
         # Make main row expand to take up room
         self.root.grid_rowconfigure(0, weight=1)
 
+        # Make sidebar that displays categories and projects
         self.sidebar = tk.LabelFrame(self.root, text="Projects")
-        self.sidebar.grid_rowconfigure(0, weight=1)
         self.sidebar.grid(row=0, column=0, sticky = tk.N+tk.S)
+        self.sidebar.grid_rowconfigure(0, weight=1)
 
         self.category_scroller = CategoryScroller(self.sidebar, lambda s: print(s.name))
         self.category_scroller.grid(row=0, column=0, sticky = tk.N + tk.S)
         self.category_scroller.showCategories(self.db.get_all())
+
+        # Make main window that shows the selected project
+        self.root.grid_columnconfigure(1, weight=1)
+        self.main_window = tk.LabelFrame(self.root, text="No project selected")
+        self.main_window.grid(row=0, column=1, sticky=tk.N+tk.S+tk.E+tk.W)
