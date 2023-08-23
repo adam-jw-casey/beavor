045722b0a1311be7182ab9486af85b5224e75e0b
Adam Casey <adam.jw.casey@gmail.com>
2023-08-22 22:58:34 -0600

    Break out DateEntry into its own file

--- beavor/widgets.py
+++ beavor/widgets.py
@@ -7,10 +7,11 @@ import datetime
 import sys
 from typing import List, Any, Optional
 
-from .backend import green_red_scale, DatabaseManager, Task, PyDueDate, today_date, format_date, parse_date
+from .backend import green_red_scale, DatabaseManager, Task, PyDueDate, today_date, parse_date
 from .ScrollFrame import ScrollFrame
 from .Timer import Timer
 from .CompletingComboBox import CompletingComboBox
+from .DateEntry import DateEntry
 
 ###########################################
 #Readability / coding style / maintainability
@@ -30,37 +31,6 @@ from .CompletingComboBox import CompletingComboBox
     # todo user-customizable settings (like font size, calendar colourscale) -> This could write to external file, read at startup?
 
 ###########################################
-
-class DateEntry(tk.Entry):
-  def __init__(self, parentFrame, notify):
-    super().__init__(parentFrame)
-    self.notify = notify
-    self.bind("<Tab>", lambda _: self.convertDate)
-
-  def convertDate(self) -> None:
-    dateStr = self.get()
-    convertedDate = ""
-
-    try:
-      parse_date(dateStr)
-      convertedDate = dateStr
-    except ValueError:
-      try:
-        #eg. Jan 1, 21
-        convertedDate = format_date(datetime.datetime.strptime(dateStr, "%b %d, %y"))
-      except ValueError:
-        #Date string doesn't match
-        try:
-          #Try to add the current year
-          #eg. Jan 1
-          convertedDate = format_date(datetime.datetime.strptime(dateStr, "%b %d").replace(year = today_date().year))
-        except ValueError:
-          #Date really doesn't match
-          self.notify("Can't match date format of {}".format(dateStr))
-          return
-
-    self.delete(0, tk.END)
-    self.insert(0, convertedDate)
 
 class EditingPane(tk.LabelFrame):
     def __init__(self, parent, getSelectedTask, save, notify, get_categories, newTask, deleteTask, getDefaultTask):
