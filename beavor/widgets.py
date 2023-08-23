554535ffa1c3620f93897c34898efbf5e4c5724b
Adam Casey <adam.jw.casey@gmail.com>
2023-08-22 22:53:01 -0600

    Break Timer out into its own file

--- beavor/widgets.py
+++ beavor/widgets.py
@@ -9,6 +9,7 @@ from typing import List, Any, Optional
 
 from .backend import green_red_scale, DatabaseManager, Task, PyDueDate, today_date, format_date, parse_date
 from .ScrollFrame import ScrollFrame
+from .Timer import Timer
 
 ###########################################
 #Readability / coding style / maintainability
@@ -28,63 +29,6 @@ from .ScrollFrame import ScrollFrame
     # todo user-customizable settings (like font size, calendar colourscale) -> This could write to external file, read at startup?
 
 ###########################################
-
-class Timer(tk.Frame):
-
-    def __init__(self, parent: tk.Frame | tk.LabelFrame, getSelectedTask, save, setUsedTime, notify):
-        super().__init__(parent)
-
-        self.notify = notify
-
-        self.timeLabel = tk.Label(self, text=str(datetime.timedelta()))
-        self.timeLabel.grid(row=0, column=1)
-
-        self.timeButton = tk.Button(self, text="Start", command=lambda: self.toggleTimer(getSelectedTask()))
-        self.timeButton.grid(row=0, column=0)
-        self.timing = False
-
-        self.save = save
-        self.setUsedTime = setUsedTime
-
-    def toggleTimer(self, selected_task) -> None:
-      if not self.timing:
-        self.start(selected_task)
-      else:
-        self.stop()
-        self.save()
-
-    def start(self, task) -> None:
-      if task is None:
-        self.notify("Cannot time an empty task")
-        return
-
-      self.timeButton.config(text="Stop")
-      self.startTime = datetime.datetime.now()
-      self.initialTime = datetime.timedelta(minutes=(task.time_used or 0))
-
-      self.timing = True
-      self._keep_displayed_time_updated()
-
-    def stop(self):
-        if self.timing:
-            self.timeButton.config(text="Start")
-            self.timing = False
-            self.setUsedTime(round(self.timerVal.total_seconds()/60))
-
-    def setTime(self, time: datetime.timedelta):
-        self.timerVal = time
-        self.timeLabel.config(text=str(time).split('.',2)[0])
-
-    def _keep_displayed_time_updated(self):
-        if self.timing:
-            runTime = datetime.datetime.now() - self.startTime
-
-            self.setTime((runTime + self.initialTime))
-
-            self.after(1000, self._keep_displayed_time_updated)
-
-    class EmptyTaskError(Exception):
-      pass
 
 class DateEntry(tk.Entry):
   def __init__(self, parentFrame, notify):
