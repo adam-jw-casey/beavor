e893f6cf83c731aca02d65d8de6d1a8ed760ca2e
Adam Casey <adam.casey@cslabs.com>
2023-08-31 20:23:43 -0700

    Remove font passed to Calendar

--- beavor/widgets/Calendar.py
+++ beavor/widgets/Calendar.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import tkinter.font
 import datetime
 
 from .SensibleReturnWidget import SensibleReturnWidget, LabelSR
@@ -10,7 +11,7 @@ from typing import Any
 # todo Days of the week shown should be user-configurable (M-F vs. student schedule lol, or freelance).
 # Set up the calendar display to show estimated workload each day for a several week forecast
 class Calendar(tk.LabelFrame, SensibleReturnWidget):
-    def __init__(self, parentFrame, parentFont):
+    def __init__(self, parentFrame):
         super().__init__(parentFrame, text="Calendar", padx=4, pady=4)
 
         self.numweeks = 4
@@ -22,7 +23,7 @@ class Calendar(tk.LabelFrame, SensibleReturnWidget):
         for i, day in enumerate(["Mon", "Tue", "Wed", "Thu", "Fri"]):
             LabelSR(
                 self,
-                font=parentFont + ("bold",),
+                font=tk.font.nametofont("TkDefaultFont"),
                 text=day
             ).grid(row=0, column=i, padx=4, pady=4)
 
@@ -34,11 +35,9 @@ class Calendar(tk.LabelFrame, SensibleReturnWidget):
                 #Alternate date labels and workloads
                 thisDay["DateLabel"] = LabelSR(
                     self,
-                    font=parentFont
                 ).grid(row=2*week + 1, column=dayNum, padx=4, pady=4)
                 thisDay["LoadLabel"] = LabelSR(
                     self,
-                    font=parentFont
                 ).grid(row=2*week + 2, column=dayNum, padx=4, pady=4)
                 thisWeek.append(thisDay)
             self.calendar.append(thisWeek)
