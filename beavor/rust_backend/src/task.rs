038e62cb3e32168dd1c11d0a771f4664245bf7d6
Adam Casey <adam.jw.casey@gmail.com>
2023-09-06 16:46:50 -0600

    Fix workload calculation and use in python GUI. Days off implementation untested.

--- beavor/rust_backend/src/task.rs
+++ beavor/rust_backend/src/task.rs
@@ -22,7 +22,7 @@ pub struct Task{
     #[pyo3(get, set)]
     pub finished:         String, // TODO It's inexcusable that this is a string and not an Enum
     #[pyo3(get, set)]
-    pub task_name:        String,
+    pub task_name:        String, // TODO this should just be `name`
     #[pyo3(get)]
     pub _time_budgeted:   u32,
     #[pyo3(get, set)]
@@ -65,13 +65,17 @@ impl IntoIterator for Task{
     fn into_iter(self) -> Self::IntoIter {
         DateIterator{
             prev: self.first_available_date(),
+            next: self.first_available_date(),
             last: self.last_available_date(),
         }
     }
 }
 
+/// This stores more state than necessary but I don't feel like optimizing it and I doubt it'll be
+/// a bottleneck anytime soon
 pub struct DateIterator{
     prev: NaiveDate,
+    next: NaiveDate,
     last: Option<NaiveDate>,
 }
 
@@ -81,8 +85,9 @@ impl Iterator for DateIterator{
     fn next(&mut self) -> Option<Self::Item> {
         match self.last{
             Some(date) => {
-                self.prev = self.prev.succ_opt().expect("This panics on huge dates");
-                Some(self.prev).filter(|d| *d != date)
+                self.prev = self.next;
+                self.next = self.prev.succ_opt().expect("This panics on huge dates");
+                Some(self.prev).filter(|d| *d <= date)
             },
             None => None,
         }
@@ -99,5 +104,63 @@ impl Task{
     #[setter]
     fn set_due_date(&mut self, due_date: PyDueDate){
         self.due_date = (&due_date).into()
+    }
+}
+
+#[cfg(test)]
+#[allow(deprecated)]
+#[allow(clippy::zero_prefixed_literal)]
+mod tests{
+    use super::*;
+
+    #[test]
+    fn test_task_date_iteration() {
+        let task_date = Task{
+            category: "".to_string(),
+            finished: "O".to_string(),
+            task_name: "".to_string(),
+            _time_budgeted: 0,
+            time_needed: 0,
+            time_used: 0,
+            notes: "".to_string(),
+            date_added: NaiveDate::from_ymd(3000, 01, 01),
+            next_action_date: NaiveDate::from_ymd(3000, 01, 01),
+            due_date: DueDate::Date(NaiveDate::from_ymd(3000, 01, 03)),
+            id: None,
+        };
+
+        assert_eq!(task_date.into_iter().count(), 3);
+
+        let task_asap = Task{
+            category: "".to_string(),
+            finished: "O".to_string(),
+            task_name: "".to_string(),
+            _time_budgeted: 0,
+            time_needed: 0,
+            time_used: 0,
+            notes: "".to_string(),
+            date_added: NaiveDate::from_ymd(3000, 01, 01),
+            next_action_date: NaiveDate::from_ymd(3000, 01, 01),
+            due_date: DueDate::ASAP,
+            id: None,
+        };
+
+        assert_eq!(task_asap.into_iter().count(), 1);
+
+        let task_none = Task{
+            category: "".to_string(),
+            finished: "O".to_string(),
+            task_name: "".to_string(),
+            _time_budgeted: 0,
+            time_needed: 0,
+            time_used: 0,
+            notes: "".to_string(),
+            date_added: NaiveDate::from_ymd(3000, 01, 01),
+            next_action_date: NaiveDate::from_ymd(3000, 01, 01),
+            due_date: DueDate::NONE,
+            id: None,
+        };
+
+        assert_eq!(task_none.into_iter().count(), 0);
     }
 }
