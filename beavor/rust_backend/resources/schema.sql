e34019309d861ff3579744a259ae631802ed2c0e
Adam Casey <adam.jw.casey@gmail.com>
2023-09-07 10:03:06 -0700

    Update schema to use primary key correctly

--- beavor/rust_backend/resources/schema.sql
+++ beavor/rust_backend/resources/schema.sql
@@ -1,42 +1,49 @@
 PRAGMA foreign_keys = ON;
 
 CREATE TABLE IF NOT EXISTS tasks(
+	TaskID		  INTEGER NOT NULL PRIMARY KEY,
 	Name		  TEXT,
 	Finished      	  BOOLEAN NOT NULL CHECK (Finished IN (0,1)),
 	TimeBudgeted  	  INTEGER,
 	TimeNeeded    	  INTEGER,
 	TimeUsed      	  INTEGER,
 	Available     	  TEXT, -- Either a date, Any, or Deliverable
+	DueDeliverable	  INTEGER,
 	PrereqDeliverable INTEGER,
 	Notes		  TEXT,
 	DateAdded     	  TEXT,
-	FOREIGN KEY (PrereqDeliverable) REFERENCES deliverables (rowid)
+	FOREIGN KEY (DueDeliverable) REFERENCES deliverables (DeliverableID),
+	FOREIGN KEY (PrereqDeliverable) REFERENCES deliverables (DeliverableID)
 	);
 
 CREATE TABLE IF NOT EXISTS projects(
+	ProjectID     INTEGER NOT NULL PRIMARY KEY,
 	Name	      TEXT,
 	Category      INTEGER,
 	UNIQUE(Name, Category),
-	FOREIGN KEY (Category) REFERENCES categories (rowid)
+	FOREIGN KEY (Category) REFERENCES categories (CategoryID)
 	);
 
 CREATE TABLE IF NOT EXISTS deliverables(
+	DeliverableID INTEGER NOT NULL PRIMARY KEY,
 	Name	      TEXT,
 	Project       INTEGER,
 	DueDate	      TEXT, --an ISO date, None, or ASAP
 	Notes	      TEXT,
-	FOREIGN KEY (Project) REFERENCES projects (rowid)
+	FOREIGN KEY (Project) REFERENCES projects (ProjectID)
 	);
 
 CREATE TABLE IF NOT EXISTS externals(
+	ExternalID      INTEGER NOT NULL PRIMARY KEY,
 	Name		TEXT,
 	Link	      	TEXT,
 	DeliverableID	INTEGER,
 	TaskID		INTEGER,
-	FOREIGN KEY (DeliverableID) REFERENCES deliverables (rowid),
-	FOREIGN KEY (DeliverableID) REFERENCES tasks (rowid)
+	FOREIGN KEY (DeliverableID) REFERENCES deliverables (DeliverableID),
+	FOREIGN KEY (DeliverableID) REFERENCES tasks (TaskID)
 	);
 
 CREATE TABLE IF NOT EXISTS categories(
+	CategoryID    INTEGER NOT NULL PRIMARY KEY,
 	Name	      TEXT UNIQUE
 	)
