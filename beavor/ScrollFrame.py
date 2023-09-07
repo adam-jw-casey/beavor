359f29564d9b16701d299937a7272fd10ed67acb
Adam Casey <adam.jw.casey@gmail.com>
2023-09-07 10:03:13 -0700

    Modify ScrollFrame to only scroll when items overflow the frame vertically

--- beavor/ScrollFrame.py
+++ beavor/ScrollFrame.py
@@ -12,14 +12,13 @@ class ScrollFrame(tk.Frame):
 
         self.vsb.pack(side="right", fill="y") #pack scrollbar to right of self
         self.canvas.pack(side="left", fill="both", expand=True) #pack canvas to left of self and expand to fil
-        self.canvas_window = self.canvas.create_window((4,4), window=self.viewPort, anchor="nw", #add view port frame to canvas
-        tags="self.viewPort")
+        self.canvas_window = self.canvas.create_window((4,4), window=self.viewPort, anchor="nw", tags="self.viewPort")
 
         self.viewPort.bind("<Configure>", self.onFrameConfigure) #bind an event whenever the size of the viewPort frame changes.
         self.canvas.bind("<Configure>", self.onCanvasConfigure) #bind an event whenever the size of the canvas frame changes.
 
         self.bind('<Enter>', self.onEnter) # bind wheel events when the cursor enters the control
-        self.bind('<Leave>', self.onLeave) # unbind wheel events when the cursorl leaves the control
+        self.bind('<Leave>', self.onLeave) # unbind wheel events when the cursor leaves the control
 
         self.onFrameConfigure(None) #perform an initial stretch on render, otherwise the scroll region has a tiny border until the first resize
 
@@ -33,15 +32,19 @@ class ScrollFrame(tk.Frame):
         self.canvas.itemconfig(self.canvas_window, width = canvas_width) #whenever the size of the canvas changes alter the window region respectively.
 
     def onMouseWheel(self, event: tk.Event):  # cross platform scroll wheel event
-        if platform.system() == 'Windows':
-            self.canvas.yview_scroll(int(-1* (event.delta/120)), "units")
-        elif platform.system() == 'Darwin':
-            self.canvas.yview_scroll(int(-1 * event.delta), "units")
-        else:
-            if event.num == 4:
-                self.canvas.yview_scroll( -1, "units" )
-            elif event.num == 5:
-                self.canvas.yview_scroll( 1, "units" )
+        canvas_height = self.canvas.winfo_height()
+        rows_height = self.canvas.bbox("all")[3]
+
+        if rows_height > canvas_height:
+            if platform.system() == 'Windows':
+                self.canvas.yview_scroll(int(-1* (event.delta/120)), "units")
+            elif platform.system() == 'Darwin':
+                self.canvas.yview_scroll(int(-1 * event.delta), "units")
+            else:
+                if event.num == 4:
+                    self.canvas.yview_scroll( -1, "units" )
+                elif event.num == 5:
+                    self.canvas.yview_scroll( 1, "units" )
 
     def onEnter(self, _): # bind wheel events when the cursor enters the control
         if platform.system() == 'Linux':
@@ -50,10 +53,9 @@ class ScrollFrame(tk.Frame):
         else:
             self.canvas.bind_all("<MouseWheel>", self.onMouseWheel)
 
-    def onLeave(self, _): # unbind wheel events when the cursorl leaves the control
+    def onLeave(self, _): # unbind wheel events when the cursor leaves the control
         if platform.system() == 'Linux':
             self.canvas.unbind_all("<Button-4>")
             self.canvas.unbind_all("<Button-5>")
         else:
             self.canvas.unbind_all("<MouseWheel>")
-
