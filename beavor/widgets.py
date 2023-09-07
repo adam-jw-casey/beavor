eef711fc0eb6d820d4816da2019fdd74d1f396a5
Adam Casey <adam.jw.casey@gmail.com>
2023-09-07 10:03:19 -0700

    Fix bug in click tracking code for context menus and editable label

--- beavor/widgets.py
+++ beavor/widgets.py
@@ -87,7 +87,7 @@ class ContextMenuSpawner:
         self.ctx_menu.bind("<2>", lambda _: self.click_tracker.unbind(), "+")
         self.ctx_menu.bind("<3>", lambda _: self.click_tracker.unbind(), "+")
 
-        self.click_tracker = TrackOutsideClick(self.parents[0])
+        self.click_tracker = TrackOutsideClick(self.parents)
         self.click_tracker.bind(self.destroy_context_menu)
 
     def destroy_context_menu(self):
@@ -106,7 +106,7 @@ class EditableLabel(tk.Frame):
 
         self.edit_box = tk.Entry(self)
         self.edit_box.bind("<Return>", lambda _: self.save())
-        self.click_tracker = TrackOutsideClick(self.edit_box)
+        self.click_tracker = TrackOutsideClick([self.edit_box])
 
     def bind(self, *args, **kwargs):
         super().bind(*args, **kwargs)
@@ -130,6 +130,7 @@ class EditableLabel(tk.Frame):
             # move cursor to the end
             self.edit_box.icursor('end')
 
+
         self.label.grid_forget()
         self.edit_box.grid(sticky=tk.N+tk.S+tk.E+tk.W)
         self.edit_box.delete(0, tk.END)
@@ -138,39 +139,44 @@ class EditableLabel(tk.Frame):
         self.edit_box.focus_force()
         select_all()
 
-        self.click_tracker.bind(self.save)
+        self.click_tracker.bind(self.save, ignore_self_click=True)
 
 class TrackOutsideClick:
     """
     Register when click events occur outside of a given widget
     """
-    def __init__(self, domain: tk.Widget):
+    def __init__(self, domain: list[tk.Widget]):
         self.funcid1: str
         self.funcid2: str
         self.funcid3: str
         self.funcid4: str
 
-        self.domain: tk.Widget = domain
+        self.domain: list[tk.Widget] = domain
         self.bound: bool = False
 
-    def bind(self, callback: Callable[[], Optional[str]]):
+    def bind(self, callback: Callable[[], Optional[str]], ignore_self_click=False):
         # Only allow a single binding, to avoid losing ids of old bindings
         # and leaving them dangling
         if self.bound:
             self.unbind()
         self.bound = True
 
-        wrapped_callback = lambda e: (callback() if e.widget != self.domain else None)
+        # Optionally ignore clicks that occur on domain widget
+        wrapped_callback: Callable[[tk.Event], Optional[str]]
+        if ignore_self_click:
+            wrapped_callback = lambda e: (callback() if e.widget not in self.domain else None)
+        else:
+            wrapped_callback = lambda _: callback()
 
-        self.funcid1 = self.domain.winfo_toplevel().bind("<1>", wrapped_callback, "+")
-        self.funcid2 = self.domain.winfo_toplevel().bind("<2>", wrapped_callback, "+")
-        self.funcid3 = self.domain.winfo_toplevel().bind("<3>", wrapped_callback, "+")
-        self.funcid4 = self.domain.winfo_toplevel().bind("<Configure>", lambda e: callback if e.widget != self.domain else None, "+")
+        self.funcid1 = self.domain[0].winfo_toplevel().bind("<1>", wrapped_callback, "+")
+        self.funcid2 = self.domain[0].winfo_toplevel().bind("<2>", wrapped_callback, "+")
+        self.funcid3 = self.domain[0].winfo_toplevel().bind("<3>", wrapped_callback, "+")
+        self.funcid4 = self.domain[0].winfo_toplevel().bind("<Configure>", lambda e: (callback() if e.widget == self.domain[0].winfo_toplevel() else None), "+")
 
     def unbind(self):
-        self.domain.winfo_toplevel().unbind("<1>", self.funcid1)
-        self.domain.winfo_toplevel().unbind("<2>", self.funcid2)
-        self.domain.winfo_toplevel().unbind("<3>", self.funcid3)
-        self.domain.winfo_toplevel().unbind("<Configure>", self.funcid4)
+        self.domain[0].winfo_toplevel().unbind("<1>", self.funcid1)
+        self.domain[0].winfo_toplevel().unbind("<2>", self.funcid2)
+        self.domain[0].winfo_toplevel().unbind("<3>", self.funcid3)
+        self.domain[0].winfo_toplevel().unbind("<Configure>", self.funcid4)
 
         self.bound = False
