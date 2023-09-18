4d7bd69d1f6a4dc17ddb7c1cbae84015d0306f25
Adam Casey <adam.jw.casey@gmail.com>
2023-09-18 11:33:21 -0700

    Refactor Calendar

--- beavor/widgets/Calendar.py
+++ beavor/widgets/Calendar.py
@@ -2,7 +2,7 @@ import tkinter as tk
 import tkinter.font
 import datetime
 
-from .SensibleReturnWidget import SensibleReturnWidget, LabelSR
+from .SensibleReturnWidget import SensibleReturnWidget, LabelSR, FrameSR
 from ..utils.ContextMenuSpawner import ContextMenuSpawner
 from ..backend import green_red_scale, today_date, Schedule
 from typing import Any, Callable
@@ -21,23 +21,14 @@ class Calendar(tk.LabelFrame, SensibleReturnWidget):
         on_click_date: Callable[[datetime.date], None] = lambda _: None,
         numweeks=4
     ):
-        def context_menu_builder(date: datetime.date) -> tk.Menu:
-            ctx = tk.Menu(self, tearoff=0)
-
-            ctx.add_command(label="Mark vacation", command=lambda d=date: mark_vacation(d))
-            ctx.add_command(label="Unmark vacation", command=lambda d=date: unmark_vacation(d))
-
-            return ctx
-
         super().__init__(parentFrame, text="Calendar", padx=4, pady=4)
 
         self.numweeks = numweeks
 
         #Build the calendar out of labels
-        self.calendar = []
+        self.calendar: list[list[DayDisplay]] = []
 
-        # TODO the days should have their own Widget class. Calendar shouldn't have so much low-level tinkering
-        #Add day of week names at top, but won't change so don't save
+        # Add day of week headers
         for i, day in enumerate(["Mon", "Tue", "Wed", "Thu", "Fri"]):
             LabelSR(
                 self,
@@ -46,23 +37,17 @@ class Calendar(tk.LabelFrame, SensibleReturnWidget):
             ).grid(row=0, column=i, padx=4, pady=4)
 
         for week in range(self.numweeks):
-            thisWeek = []
+            thisWeek:list[DayDisplay] = []
+
             for dayNum in range(5):
-                thisDay: dict[str, Any] = {}
-                #Alternate date labels and workloads
-                thisDay["DateLabel"] = LabelSR(
+                day = DayDisplay(
                     self,
-                ).grid(row=2*week + 1, column=dayNum, padx=4, pady=4
-                ).bind("<1>", lambda _, d=thisDay: on_click_date(d["Date"]))
+                    mark_vacation,
+                    unmark_vacation,
+                    on_click_date
+                ).grid(row=week + 1, column=dayNum, padx=4, pady=4)
+                thisWeek.append(day)
 
-                thisDay["LoadLabel"] = LabelSR(
-                    self,
-                ).grid(row=2*week + 2, column=dayNum, padx=4, pady=4
-                ).bind("<1>", lambda _, d=thisDay: on_click_date(d["Date"]))
-
-                ContextMenuSpawner([thisDay["DateLabel"], thisDay["LoadLabel"]], lambda d=thisDay: context_menu_builder(d["Date"]))
-
-                thisWeek.append(thisDay)
             self.calendar.append(thisWeek)
 
     # todo this function isn't great but it seems to work
@@ -73,28 +58,65 @@ class Calendar(tk.LabelFrame, SensibleReturnWidget):
         hoursLeftToday = max(0, min(7, 16 - (datetime.datetime.now().hour + datetime.datetime.now().minute/60)))
 
         for week in range(self.numweeks):
-            for day in range(5):
-                thisDay = self.calendar[week][day]
-                thisDate = thisMonday + datetime.timedelta(days=day, weeks=week)
-                thisDay["Date"] = thisDate
-                thisDay["DateLabel"].config(text=thisDate.strftime("%b %d"))
+            for week_day in range(5):
+                day = self.calendar[week][week_day]
+                date = thisMonday + datetime.timedelta(days=week_day, weeks=week)
 
-                # TODO should also highlight the day that is selected / filterred to
-                if thisDate >= today:
-                    if thisDate == today:
-                        thisDay["DateLabel"].config(bg="lime")
-                    elif not schedule.is_work_day(thisDay["Date"]):
-                        thisDay["DateLabel"].config(bg="RoyalBlue")
-                        thisDay["LoadLabel"].config(bg="gray85", text="")
-                        continue
-                    else:
-                        thisDay["DateLabel"].config(bg="gray85")
+                day.update(schedule, date, today, hoursLeftToday)
 
-                    hoursThisDay = schedule.workload_on_day(thisDate) / 60
-                    thisDay["LoadLabel"]\
-                      .config(
-                          text=str(round(hoursThisDay,1)),
-                          bg=green_red_scale(0,(8 if thisDate != today else max(0, hoursLeftToday)), hoursThisDay))
-                else:
-                    thisDay["DateLabel"].config(bg="gray85")
-                    thisDay["LoadLabel"].config(text="", bg="gray85")
+class DayDisplay(FrameSR):
+    def __init__(
+        self,
+        parent,
+        mark_vacation: Callable[[datetime.date], None] = lambda _: None,
+        unmark_vacation: Callable[[datetime.date], None] = lambda _: None,
+        on_click_date: Callable[[datetime.date], None] = lambda _: None
+    ):
+        def context_menu_builder(date: datetime.date) -> tk.Menu:
+            ctx = tk.Menu(self, tearoff=0)
+
+            ctx.add_command(label="Mark vacation", command=lambda d=date: mark_vacation(d))
+            ctx.add_command(label="Unmark vacation", command=lambda d=date: unmark_vacation(d))
+
+            return ctx
+
+        super().__init__(parent)
+
+        self.date: datetime.date
+
+        #Alternate date labels and workloads
+        self.date_label: LabelSR = LabelSR(
+            self
+        ).pack(side="top"
+        ).bind("<1>", lambda _, d=self: on_click_date(d.date))
+
+        self.load_label: LabelSR = LabelSR(
+            self
+        ).pack(side="bottom"
+        ).bind("<1>", lambda _, d=self: on_click_date(d.date))
+
+        ContextMenuSpawner([self.date_label, self.load_label], lambda d=self: context_menu_builder(d.date))
+
+    def update(self, schedule: Schedule, date: datetime.date, today: datetime.date, hoursLeftToday: float):
+        self.date = date
+        self.date_label.config(text=date.strftime("%b %d"))
+
+        # TODO should also highlight the day that is selected / filtered to
+        if date >= today:
+            if date == today:
+                self.date_label.config(bg="lime")
+            elif not schedule.is_work_day(date):
+                self.date_label.config(bg="RoyalBlue")
+                self.load_label.config(bg="gray85", text="")
+                return
+            else:
+                self.date_label.config(bg="gray85")
+
+            hoursThisDay = schedule.workload_on_day(date) / 60
+            self.load_label\
+              .config(
+                  text=str(round(hoursThisDay,1)),
+                  bg=green_red_scale(0,(8 if date != today else max(0, hoursLeftToday)), hoursThisDay))
+        else:
+            self.date_label.config(bg="gray85")
+            self.load_label.config(text="", bg="gray85")
