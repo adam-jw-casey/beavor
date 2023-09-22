41461fd8917ddf80a8cf4e643a331fbaa0c02113
Adam Casey <adam.jw.casey@gmail.com>
2023-09-22 08:10:56 -0600

    Stub in iced framework with a hello world widget

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,55 @@
+use iced::widget::{
+    column,
+    container,
+    row,
+    scrollable,
+    text,
+};
+
+use iced::{
+    Sandbox,
+    Element,
+    Settings,
+};
+
 fn main() {
-    println!("Hello, world!");
+    Beavor::run(Settings::default())
+        .expect("Application failed");
+}
+
+#[derive(Debug, Clone)]
+enum Message{
+}
+
+struct Beavor;
+
+impl Sandbox for Beavor {
+    type Message = Message;
+
+    fn new() -> Self {
+        Self{}
+    }
+
+    fn title(&self) -> String {
+        String::from("Beavor")
+    }
+
+    fn update(&mut self, message: Self::Message) {
+        match message{
+        }
+    }
+
+    fn view(&self) -> Element<'_, Self::Message> {
+        let task_scroller: Element<Message> =
+            scrollable(
+                column![
+                    text("hello"),
+                    text("world"),
+                ]
+            ).into();
+
+        let content: Element<Message> = row![task_scroller].into();
+
+        container(content).into()
+    }
 }
