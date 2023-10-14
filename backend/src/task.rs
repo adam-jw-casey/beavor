6bd2ed7d02814d05dac63f3a22e1197ee7f3d270
Adam Casey <adam.jw.casey@gmail.com>
2023-10-14 11:15:06 -0600

    Add derives for serde

--- backend/src/task.rs
+++ backend/src/task.rs
@@ -6,6 +6,7 @@ use crate::due_date::DueDate;
 pub type Id = Option<u32>;
 
 #[derive(Clone, Debug, PartialEq, Eq, Hash)]
+#[cfg_attr(feature="serde", derive(Serialize, Deserialize))]
 pub struct Task{
     pub category:         String,
     pub finished:         bool,
