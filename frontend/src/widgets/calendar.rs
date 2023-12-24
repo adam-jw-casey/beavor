41335b8e26ef63e83cb15fb89efe9cbefa707de5
Adam Casey <adam.jw.casey@gmail.com>
2023-12-23 23:20:07 -0700

    Toggle the filtered-to date in the calendar by clicking again

--- frontend/src/widgets/calendar.rs
+++ frontend/src/widgets/calendar.rs
@@ -104,7 +104,13 @@ pub fn calendar(schedule: &Schedule, state: &State) -> Element<'static, MessageW
                      Column::with_children(
                         (0..num_weeks)
                             .map(|n| *d + Days::new(7*n))
-                            .map(|d| Element::from(cal_day(d, schedule.get_time_assigned_on_day(d), Some(d) == state.filter_date, state.clicked_date.as_ref())))
+                            .map(|d| Element::from(cal_day(
+                                d,
+                                schedule.get_time_assigned_on_day(d),
+                                Some(d) == state.filter_date,
+                                state.clicked_date.as_ref(),
+                                state.filter_date.as_ref(),
+                            )))
                             .collect()
                         )
                     ]
@@ -128,7 +134,7 @@ pub fn calendar(schedule: &Schedule, state: &State) -> Element<'static, MessageW
         .into()
 }
 
-fn cal_day(day: NaiveDate, load: Option<Duration>, is_selected: bool, clicked_date: Option<&NaiveDate>) -> Element<'static, MessageWrapper>{
+fn cal_day(day: NaiveDate, load: Option<Duration>, is_selected: bool, clicked_date: Option<&NaiveDate>, filter_date: Option<&NaiveDate>) -> Element<'static, MessageWrapper>{
     MouseArea::new(
         column![
             text(
@@ -151,10 +157,16 @@ fn cal_day(day: NaiveDate, load: Option<Duration>, is_selected: bool, clicked_da
         .on_press(MessageWrapper::Calendar(Message::ClickDate(Some(day))))
         .on_release(
             if clicked_date.is_some_and(|d| *d == day){
-                MessageWrapper::Calendar(Message::FilterToDate(Some(day)))
+                // TODO what I MEAN here is "toggle" but there's no way to express that in a message at the moment
+                if filter_date.is_some_and(|d| *d == day){
+                    MessageWrapper::Calendar(Message::FilterToDate(None))
+                }else{
+                    MessageWrapper::Calendar(Message::FilterToDate(Some(day)))
+                }
             }else{
                 MessageWrapper::Calendar(Message::ClickDate(None))
-        })
+            }
+        )
         .on_right_press(
              MessageWrapper::Mutate(MutateMessage::VacationStatus(day, load.is_some()))
         )
