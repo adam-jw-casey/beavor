09899bc22a3b557b8f0131d290af4a38335fdcb6
Adam Casey <adam.jw.casey@gmail.com>
2024-01-04 21:37:08 -0700

    Make Milestone an enum to allow for anonymous instances implicitly bounding tasks that do not have milestones explicitly linked.

--- backend/src/milestone.rs
+++ backend/src/milestone.rs
@@ -1,10 +1,29 @@
+use chrono::NaiveDate;
+
 use crate::due_date::DueDate;
 use crate::task::Id;
 
+use crate::utils::format_date_borrowed;
+
 #[derive(Clone, Debug, PartialEq, Eq, Hash)]
-struct Milestone {
-    pub name:       String,
-    pub category:   String,
-    pub due_date:   DueDate,
-    pub id:         Id,
+pub enum Milestone {
+    // TODO I don't like that this duplicates the data of task.next_action_date and task.due_date
+    AnonymousStart(NaiveDate),
+    AnonymousEnd(DueDate),
+    Concrete {
+        name:     String,
+        category: String,
+        due_date: DueDate,
+        id:       Id,
+    },
+}
+
+impl Milestone {
+    #[must_use] fn get_name(&self) -> String {
+        match self {
+            Milestone::AnonymousStart(date) => format_date_borrowed(date),
+            Milestone::AnonymousEnd(due_date) => due_date.into(),
+            Milestone::Concrete { name, category: _, due_date: _, id: _ } => name.into(),
+        }
+    }
 }
